a6be971b3985da661145bc3fd0f107102d7f9507
floxydio <diooktar@gmail.com>
2023-10-29 15:42:04 +0700

    init react native

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from "react"
+
 
 export default function App() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} placeholder='Username' onChangeText={(e) => setUsername(e)} />
+      <TextInput secureTextEntry={true} style={styles.input} placeholder='Password' onChangeText={(e) => setPassword(e)} />
+      <Pressable onPress={() => alert(`Username: ${username}, password: ${password}`)} style={{ backgroundColor: 'blue', padding: 10, borderRadius: 10, width: 200, marginTop: 50, alignItems: "center", height: 40 }}>
+        <Text style={{
+          color: "white",
+          fontWeight: "bold",
+        }}>Sign In</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    width: 300,
+    borderWidth: 0.3,
+    borderRadius: 10,
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
